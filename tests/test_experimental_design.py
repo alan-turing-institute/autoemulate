2399daae9bba7bc3685c3ed06f806cdbccd215a8
mastoffel <martin.adam.stoffel@gmail.com>
2023-09-15 10:41:23 +0100

    fix tests for exp design

--- tests/test_experimental_design.py
+++ tests/test_experimental_design.py
@@ -4,39 +4,35 @@ from dtemulate.experimental_design import ExperimentalDesign, LatinHypercube
 
 # Test LatinHypercube Initialisation
 @pytest.fixture
-def lh(self):
+def lh():
     return LatinHypercube([(0.0, 1.0), (10.0, 100.0)])
 
 
-def test_init(self, lh):
+def test_init(lh):
     assert lh is not None
 
 
-def test_parameters(self, lh):
+def test_parameters(lh):
     assert lh.get_n_parameters() == 2
 
 
 # Test LatinHypercube Sampling
 @pytest.fixture
-def lh_sample(self, lh):
+def lh_sample(lh):
     return lh.sample(3)
 
 
-def test_sample_shape(self, lh_sample):
+def test_sample_shape(lh_sample):
     assert lh_sample.shape == (3, 2)
 
 
-def test_sample_lower_bound_par1(self, lh_sample):
+def test_sample_lower_bound_par1(lh_sample):
     assert np.all(lh_sample[:, 0] >= 0.0)
 
 
-def test_sample_upper_bound_par1(self, lh_sample):
+def test_sample_upper_bound_par1(lh_sample):
     assert np.all(lh_sample[:, 0] <= 1.0)
 
 
-def test_sample_lower_bound_par2(self, lh_sample):
+def test_sample_lower_bound_par2(lh_sample):
     assert np.all(lh_sample[:, 1] >= 10.0)
-
-
-def test_sample_upper_bound_par2(self, lh_sample):
-    assert np.all(lh_sample[:, 1] <= 100.0)
