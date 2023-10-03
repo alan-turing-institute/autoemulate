8b43fb9c7405e736c9afc599eeda54149b283cc2
mastoffel <martin.adam.stoffel@gmail.com>
2023-10-03 09:26:07 +0100

    improve printing scores

--- autoemulate/compare.py
+++ autoemulate/compare.py
@@ -2,8 +2,8 @@ from sklearn.model_selection import KFold
 from autoemulate.experimental_design import LatinHypercube
 from autoemulate.metrics import METRIC_REGISTRY
 from autoemulate.emulators import MODEL_REGISTRY
-import numpy as np
 import pandas as pd
+import numpy as np
 
 
 class AutoEmulate:
@@ -15,7 +15,11 @@ class AutoEmulate:
         self.y = None
         self.cv = None
         self.models = None
-        self.scores_df = pd.DataFrame(columns=["model", "metric", "fold", "score"]).astype({'model': 'object', 'metric': 'object', 'fold': 'int64', 'score': 'float64'})
+        self.scores_df = pd.DataFrame(
+            columns=["model", "metric", "fold", "score"]
+        ).astype(
+            {"model": "object", "metric": "object", "fold": "int64", "score": "float64"}
+        )
         self.is_set_up = False
 
     def setup(self, X, y, cv=None):
@@ -37,20 +41,32 @@ class AutoEmulate:
             model_name = type(model).__name__
             print(f"Training {model_name}...")
             self._score_model_with_cv(model)
-            
 
     def print_scores(self, model=None):
         if model is None:
-            means = self.scores_df.groupby(["model", "metric"])["score"].mean().unstack()
+            means = (
+                self.scores_df
+                .groupby(["model", "metric"])["score"]
+                .mean()
+                .unstack()
+                .reset_index()
+            )
             print("Average Scores Across All Models:")
-            print(means)
+            print(means.to_string(index=False))
         else:
             specific_model_scores = self.scores_df[self.scores_df["model"] == model]
-            folds = specific_model_scores.groupby(["metric", "fold"])["score"].mean().unstack()
+            folds = (
+                specific_model_scores.groupby(["metric", "fold"])["score"]
+                .mean()
+                .unstack()
+                .transpose()
+            )
+            folds.columns.name = None
+            folds.index.name = None
             folds.loc["Mean"] = folds.mean()
             folds.loc["Std Dev"] = folds.std()
-            print(f"Scores for {model} Across All Folds:")
-            print(folds)
+            print(f"Scores for {model} across all folds:")
+            print(folds.to_string())
 
     def _preprocess_data(self, X, y):
         self.X = np.array(X)
@@ -85,17 +101,12 @@ class AutoEmulate:
             fold_scores = self._evaluate_model(trained_model, X_test, y_test)
 
             for metric, score in fold_scores.items():
-                new_row = pd.DataFrame({
-                    "model": [model_name],
-                    "metric": [metric],
-                    "fold": [fold],  # Now correctly included
-                    "score": [score]
-                })
+                new_row = pd.DataFrame(
+                    {
+                        "model": [model_name],
+                        "metric": [metric],
+                        "fold": [fold],  # Now correctly included
+                        "score": [score],
+                    }
+                )
                 self.scores_df = pd.concat([self.scores_df, new_row], ignore_index=True)
-
-
-
-
-
-
-
