721d3d42bc8ecca7909028dd58687449d42b3faf
mastoffel <martin.adam.stoffel@gmail.com>
2023-11-08 09:23:44 +0000

    make rbf test pass

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -2,10 +2,10 @@ from sklearn.utils.estimator_checks import parametrize_with_checks, _yield_all_c
 from autoemulate.emulators import (
     RandomForest,
     GaussianProcessSk,
-    NeuralNetwork,
+    NeuralNetSk,
     GaussianProcess,
     RadialBasis,
-    SkorchMLPRegressor,
+    NeuralNetTorch,
 )
 from functools import partial
 
@@ -13,10 +13,10 @@ from functools import partial
 @parametrize_with_checks(
     [  # GaussianProcess(),
         RandomForest(random_state=42),
-        # GaussianProcessSk(random_state=1337),
-        # NeuralNetwork(random_state=13),
-        # RadialBasis(),
-        SkorchMLPRegressor(random_state=42),
+        GaussianProcessSk(random_state=1337),
+        NeuralNetSk(random_state=13),
+        RadialBasis(),
+        # NeuralNetTorch(random_state=42), # fails because it subclasses
     ]
 )
 def test_check_estimator(estimator, check):
