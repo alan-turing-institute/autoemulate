092bfe38f285e470ad615c085008626cffb3ed84
mastoffel <martin.adam.stoffel@gmail.com>
2023-11-09 10:42:30 +0000

    add datasets
    add heart datasets
    
    add three heart datasets
    
    add dataset tests

---
+++ autoemulate/datasets.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+data_dir = Path(__file__).parent.parent / "data"
+
+
+def fetch_dataset(dataset_name, train_test=False, test_size=0.2, random_state=42):
+    """
+    Fetches a dataset by name.
+
+    Parameters
+    ----------
+    dataset_name : str
+        The name of the dataset to fetch. Must be one of "atrial_cell", "four_chamber", or "circ_adapt".
+        All data from here: https://zenodo.org/records/7405335
+        "atrial_cell" is the ionic atrial cell model data from LH sampling.
+        "four_chamber" is the 4-chamber model without LH sampling.
+        "circ_adapt" is the circulatory adaptation model also from LH sampling.
+    train_test : bool, optional
+        If True, returns the dataset split into training and testing sets.
+        If False, returns the entire dataset. Default is False.
+    test_size : float, optional
+        The proportion of the dataset to include in the test split.
+        Only used if train_test is True. Default is 0.2.
+    random_state : int, optional
+        Controls the randomness of the dataset split.
+        Only used if train_test is True. Default is 42.
+
+    Returns
+    -------
+    X : ndarray, shape (n_samples, n_features)
+        Input data.
+    y : ndarray, shape (n_samples, n_outputs)
+        Output data.
+    """
+
+    data_dir_dataset = data_dir / dataset_name
+    X = pd.read_csv(data_dir_dataset / "parameters.csv").to_numpy()
+    y = pd.read_csv(data_dir_dataset / "outputs.csv").to_numpy()
+
+    if train_test:
+        return train_test_split(X, y, test_size=test_size, random_state=random_state)
+    else:
+        return X, y
