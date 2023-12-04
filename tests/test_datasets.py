16a71a765c00aeb17c6fd322fcd72f40bcba2f9e
mastoffel <martin.adam.stoffel@gmail.com>
2023-12-04 08:02:20 +0000

    update dataset tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,16 +1,18 @@
 import pytest
-from autoemulate.datasets import fetch_dataset
+from autoemulate.datasets import fetch_cardiac_data
 
 
 def test_valid_dataset_name():
     for name in ["atrial_cell", "four_chamber", "circ_adapt"]:
-        X, y = fetch_dataset(name)
+        X, y = fetch_cardiac_data(name)
         assert X is not None
         assert y is not None
 
 
 def test_train_test_split():
-    X_train, X_test, y_train, y_test = fetch_dataset("atrial_cell", train_test=True)
+    X_train, X_test, y_train, y_test = fetch_cardiac_data(
+        "atrial_cell", train_test=True
+    )
     assert len(X_train) > 0
     assert len(X_test) > 0
     assert len(y_train) > 0
@@ -19,7 +21,7 @@ def test_train_test_split():
 
 def test_test_size():
     for size in [0.1, 0.5]:
-        X_train, X_test, _, _ = fetch_dataset(
+        X_train, X_test, _, _ = fetch_cardiac_data(
             "atrial_cell", train_test=True, test_size=size
         )
         assert len(X_test) / (len(X_train) + len(X_test)) == pytest.approx(size)
