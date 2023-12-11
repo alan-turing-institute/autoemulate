d4481f6fea66d513ebc6a64f657b03f8cc4d18b6
mastoffel <martin.adam.stoffel@gmail.com>
2023-12-11 15:15:45 +0000

    extend tests

--- autoemulate/save.py
+++ autoemulate/save.py
@@ -9,58 +9,50 @@ from autoemulate.utils import get_model_name
 class ModelSerialiser:
     def save_model(self, model, path):
         """Saves a model + metadata to disk."""
-        # Save model
+        # model
         joblib.dump(model, path)
 
-        # Create metadata
+        # metadata
         meta = {
             "model": get_model_name(model),
             "scikit-learn": sklearn.__version__,
             "numpy": np.__version__,
         }
 
-        # Save metadata
         with open(self.get_meta_path(path), "w") as f:
             json.dump(meta, f)
 
     def load_model(self, path):
         """Loads a model from disk and checks version."""
-        # Load model
         model = joblib.load(path)
-
-        # Create metadata file path
         meta_path = self.get_meta_path(path)
 
         if not os.path.exists(meta_path):
             raise FileNotFoundError(f"Metadata file {meta_path} not found.")
 
-        # Load metadata
         with open(meta_path, "r") as f:
             meta = json.load(f)
 
         sklearn_version = meta.get("scikit-learn", None)
         numpy_version = meta.get("numpy", None)
 
-        if sklearn_version and sklearn_version != sklearn.__version__:
-            print(
-                f"Warning: scikit-learn version mismatch. Expected {sklearn_version}, found {sklearn.__version__}"
-            )
-
-        if numpy_version and numpy_version != np.__version__:
-            print(
-                f"Warning: numpy version mismatch. Expected {numpy_version}, found {np.__version__}"
-            )
+        for module, version, actual_version in [
+            ("scikit-learn", sklearn_version, sklearn.__version__),
+            ("numpy", numpy_version, np.__version__),
+        ]:
+            if version and version != actual_version:
+                print(
+                    f"Warning: {module} version mismatch. Expected {version}, found {actual_version}"
+                )
 
         return model
 
     def get_meta_path(self, path):
-        """Returns the path to the metadata file."""
-        base, ext = os.path.splitext(path)
-        # if path has extenstion, replace it with _meta.json
-        # otherwise, append _meta.json
-        if ext:
-            meta_path = f"{base}_meta.json"
-        else:
-            meta_path = f"{base}_meta{ext}.json"
+        """Returns the path to the metadata file.
 
+        If the path has an extension, it is replaced with _meta.json.
+        Otherwise, _meta.json is appended to the path.
+        """
+        base, ext = os.path.splitext(path)
+        meta_path = f"{base}_meta.json" if ext else f"{base}_meta{ext}.json"
         return meta_path
--- tests/test_save.py
+++ tests/test_save.py
@@ -2,6 +2,7 @@ import pytest
 from autoemulate.save import ModelSerialiser
 from sklearn.ensemble import RandomForestRegressor
 import os
+import json
 
 
 @pytest.fixture
@@ -14,16 +15,44 @@ def model():
     return RandomForestRegressor()
 
 
-def test_save_model(model_serialiser, model):
-    path = "test_model.joblib"
-    model_serialiser.save_model(model, path)
-    assert os.path.exists(path)
-    os.remove(path)
+@pytest.fixture
+def test_path():
+    return "test_model.joblib"
 
 
-def test_load_model(model_serialiser, model):
-    path = "test_model.joblib"
-    model_serialiser.save_model(model, path)
-    loaded_model = model_serialiser.load_model(path)
+def test_save_model(model_serialiser, model, test_path):
+    model_serialiser.save_model(model, test_path)
+    assert os.path.exists(test_path)
+    assert os.path.exists(model_serialiser.get_meta_path(test_path))
+
+    with open(model_serialiser.get_meta_path(test_path), "r") as f:
+        meta = json.load(f)
+    assert "model" in meta
+    assert "scikit-learn" in meta
+    assert "numpy" in meta
+
+    os.remove(test_path)
+    os.remove(model_serialiser.get_meta_path(test_path))
+
+
+def test_load_model(model_serialiser, model, test_path):
+    model_serialiser.save_model(model, test_path)
+    loaded_model = model_serialiser.load_model(test_path)
     assert isinstance(loaded_model, type(model))
-    os.remove(path)
+    os.remove(test_path)
+    os.remove(model_serialiser.get_meta_path(test_path))
+
+
+def test_load_model_with_missing_meta_file(model_serialiser, model, test_path):
+    model_serialiser.save_model(model, test_path)
+    os.remove(model_serialiser.get_meta_path(test_path))
+    with pytest.raises(FileNotFoundError):
+        model_serialiser.load_model(test_path)
+    os.remove(test_path)
+
+
+def test_invalid_file_path(model_serialiser, model):
+    with pytest.raises(Exception):
+        model_serialiser.save_model(model, "/invalid/path/model.joblib")
+    with pytest.raises(Exception):
+        model_serialiser.load_model("/invalid/path/model.joblib")
