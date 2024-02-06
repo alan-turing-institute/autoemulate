3d96f62e653391e32eab94d4cc9857d955ba91e3
mastoffel <martin.adam.stoffel@gmail.com>
2024-02-06 10:51:50 +0000

    add tests for plotting

--- autoemulate/plotting.py
+++ autoemulate/plotting.py
@@ -3,15 +3,13 @@ import numpy as np
 from sklearn.metrics import PredictionErrorDisplay
 
 
-def validate_inputs(cv_results, y, model_name):
-    """Validates the inputs for plotting.
+def validate_inputs(cv_results, model_name):
+    """Validates cv_results and model_name for plotting.
 
     Parameters
     ----------
     cv_results : dict
         A list of cross-validation results for each model.
-    y : array-like, shape (n_samples, n_outputs)
-        Simulation output.
     model_name : str
         The name of a model to plot.
     """
@@ -25,15 +23,16 @@ def validate_inputs(cv_results, y, model_name):
             )
 
 
-def check_multi_output(y, output_index):
+def check_multioutput(y, output_index):
+    """Checks if y is multi-output and if the output_index is valid."""
     if y.ndim > 1:
-        if (output_index > y.shape[1]) | (output_index < 0):
+        if (output_index > y.shape[1] - 1) | (output_index < 0):
             raise ValueError(
                 f"Output index {output_index} is out of range. The index should be between 0 and {y.shape[1] - 1}."
             )
         print(
-            f"""Multi-output detected. Plotting only the output variable with index {output_index}. 
-To plot other outputs, set `output_index` to the desired index."""
+            f"""Multiple outputs detected. Plotting the output variable with index {output_index}. 
+To plot other outputs, set `output_index` argument to the desired index."""
         )
 
 
@@ -48,7 +47,7 @@ def plot_single_fold(
     annotation=" ",
     output_index=0,
 ):
-    """Plots a single fold for a given model.
+    """Plots a single cv fold for a given model.
 
     Parameters
     ----------
@@ -64,11 +63,13 @@ def plot_single_fold(
         The index of the fold to plot.
     ax : matplotlib.axes.Axes
         The axes on which to plot the results.
-    annotation : str, optional
+    plot_type : str, optional
         The type of plot to draw:
         “actual_vs_predicted” draws the observed values (y-axis) vs. the predicted values (x-axis) (default).
         “residual_vs_predicted” draws the residuals, i.e. difference between observed and predicted values,
         (y-axis) vs. the predicted values (x-axis).
+    annotation : str, optional
+        The annotation to add to the plot title. Default is an empty string.
     output_index : int, optional
         The index of the output to plot. Default is 0.
     """
@@ -84,11 +85,20 @@ def plot_single_fold(
     if y.ndim > 1:
         true_values = true_values[:, output_index]
         predicted_values = predicted_values[:, output_index]
-
+    # plot
     display = PredictionErrorDisplay.from_predictions(
         y_true=true_values, y_pred=predicted_values, kind=plot_type, ax=ax
     )
     ax.set_title(f"{model_name} - {annotation}: {fold_index}")
+    # add R2 score to the plot
+    ax.text(
+        0.05,
+        0.95,
+        f"$R^2$ = {cv_results[model_name]['test_r2'][fold_index]:.3f}",
+        horizontalalignment="left",
+        verticalalignment="top",
+        transform=ax.transAxes,
+    )
 
 
 def plot_best_fold_per_model(
@@ -100,7 +110,7 @@ def plot_best_fold_per_model(
     figsize=None,
     output_index=0,
 ):
-    """Plots the best fold for each model in cv_results.
+    """Plots results of the best (highest R^2) cv-fold for each model in cv_results.
 
     Parameters
     ----------
@@ -116,7 +126,9 @@ def plot_best_fold_per_model(
         The type of plot to draw:
         “actual_vs_predicted” or “residual_vs_predicted”.
     figsize : tuple, optional
-        Overrides the default figure size.
+        Width, height in inches. Overrides the default figure size.
+    output_index : int, optional
+        The index of the output to plot. Default is 0.
     """
 
     n_models = len(cv_results)
@@ -177,6 +189,8 @@ def plot_model_folds(
         “actual_vs_predicted” or “residual_vs_predicted”.
     figsize : tuple, optional
         Overrides the default figure size.
+    output_index : int, optional
+        The index of the output to plot. Default is 0.
     """
 
     n_folds = len(cv_results[model_name]["estimator"])
@@ -238,8 +252,8 @@ def plot_results(
         Overrides the default figure size.
     """
 
-    validate_inputs(cv_results, y, model_name)
-    check_multi_output(y, output_index)
+    validate_inputs(cv_results, model_name)
+    check_multioutput(y, output_index)
 
     if model_name:
         plot_model_folds(
