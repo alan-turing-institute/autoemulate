c36ecc8c24d1bffee81add5ca42d99bd230058a2
mastoffel <martin.adam.stoffel@gmail.com>
2024-02-12 09:35:53 +0000

    add tests for hold-out set splitting

--- tests/test_cross_validate.py
+++ tests/test_cross_validate.py
@@ -6,14 +6,19 @@ import pandas as pd
 import pytest
 from sklearn.datasets import make_regression
 from sklearn.model_selection import KFold
+from sklearn.model_selection import PredefinedSplit
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from autoemulate.compare import AutoEmulate
 from autoemulate.cross_validate import run_cv
+from autoemulate.cross_validate import single_split
+from autoemulate.cross_validate import split_data
 from autoemulate.cross_validate import update_scores_df
 from autoemulate.emulators import RandomForest
 from autoemulate.metrics import METRIC_REGISTRY
+
 
 # import make_regression_data
 
@@ -53,3 +58,26 @@ def test_update_scores_df(cv_results):
     assert scores_df_new.shape[0] == 10
     assert scores_df_new.shape[1] == 4
     assert scores_df_new["model"][0] == "RandomForest"
+
+
+def test_single_split():
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    test_idxs = [1, 3]
+    split_index = single_split(X, test_idxs)
+
+    assert isinstance(split_index, PredefinedSplit)
+    assert np.array_equal(split_index.test_fold, [-1, 0, -1, 0])
+
+
+def test_split_data():
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    test_size = 0.2
+    random_state = 42
+    param_search = True
+
+    train_idxs, test_idxs = split_data(X, test_size, random_state, param_search)
+
+    assert isinstance(train_idxs, np.ndarray)
+    assert isinstance(test_idxs, np.ndarray)
+    assert len(train_idxs) == 3
+    assert len(test_idxs) == 1
