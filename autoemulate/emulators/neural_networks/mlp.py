28541d5b4520d12ac4e0457c5e8caf0d4641cc60
Bryan M. Li <bryanlimy@gmail.com>
2024-02-13 10:48:06 +0000

    `MLPModule` search space for `cardiac` datasets (#157)
    * fixed NeuralNetTorch initialization where module is always the class and module_ is the instance of the class. fixed 1 previsouly ignored test. hyperparameter search pass module argument properly
    
    * new construction of NeuralNetTorch passed previously ignored tests
    
    * add module__hidden_activation
    
    * separate hidden_size and hidden_layers for mlp. update mlp search space for bayes
    
    * add optimizer search space for MLP

--- autoemulate/emulators/neural_networks/mlp.py
+++ autoemulate/emulators/neural_networks/mlp.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+import numpy as np
 import torch
 from scipy.stats import loguniform
 from skopt.space import Integer
@@ -17,7 +18,9 @@ class MLPModule(TorchModule):
         input_size: int = None,
         output_size: int = None,
         random_state: int = None,
-        hidden_sizes: Tuple[int] = (100,),
+        hidden_layers: int = 1,
+        hidden_size: int = 100,
+        hidden_activation: Tuple[callable] = nn.ReLU,
     ):
         super(MLPModule, self).__init__(
             module_name="mlp",
@@ -26,32 +29,34 @@ class MLPModule(TorchModule):
             random_state=random_state,
         )
         modules = []
-        for hidden_size in hidden_sizes:
+        assert hidden_layers >= 1
+        for _ in range(hidden_layers):
             modules.append(nn.Linear(in_features=input_size, out_features=hidden_size))
-            modules.append(nn.ReLU())
+            modules.append(hidden_activation())
             input_size = hidden_size
         modules.append(nn.Linear(in_features=input_size, out_features=output_size))
         self.model = nn.Sequential(*modules)
 
     def get_grid_params(self, search_type: str = "random"):
+        param_space = {
+            "max_epochs": np.arange(10, 110, 10).tolist(),
+            "batch_size": np.arange(2, 128, 2).tolist(),
+            "module__hidden_layers": np.arange(1, 4).tolist(),
+            "module__hidden_size": np.arange(50, 250, 50).tolist(),
+            "module__hidden_activation": [
+                nn.ReLU,
+                nn.Tanh,
+                nn.Sigmoid,
+                nn.GELU,
+            ],
+            "optimizer": [torch.optim.AdamW, torch.optim.SGD],
+            "optimizer__weight_decay": (1 / 10 ** np.arange(1, 9)).tolist(),
+        }
         match search_type:
             case "random":
-                param_space = {
-                    "lr": loguniform(1e-4, 1e-2),
-                    "max_epochs": [10, 20, 30],
-                    "module__hidden_sizes": [
-                        (50,),
-                        (100,),
-                        (100, 50),
-                        (100, 100),
-                        (200, 100),
-                    ],
-                }
+                param_space |= {"lr": loguniform(1e-06, 1e-2)}
             case "bayes":
-                param_space = {
-                    "lr": Real(1e-4, 1e-2, prior="log-uniform"),
-                    "max_epochs": Integer(10, 30),
-                }
+                param_space |= {"lr": Real(1e-06, 1e-2, prior="log-uniform")}
             case _:
                 raise ValueError(f"Invalid search type: {search_type}")
 
