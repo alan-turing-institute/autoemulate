deb526b62ffd2b47cc550a22ee752d1f1611c8ec
mastoffel <martin.adam.stoffel@gmail.com>
2024-03-07 12:49:31 +0000

    fix rmse warning

--- autoemulate/metrics.py
+++ autoemulate/metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from sklearn.metrics import root_mean_squared_error
 
 
 def rmse(y_true, y_pred):
@@ -13,7 +13,7 @@ def rmse(y_true, y_pred):
     y_pred : array-like, shape (n_samples, n_outputs)
         Emulator output.
     """
-    return mean_squared_error(y_true, y_pred, squared=False)
+    return root_mean_squared_error(y_true, y_pred)
 
 
 def r2(y_true, y_pred):
