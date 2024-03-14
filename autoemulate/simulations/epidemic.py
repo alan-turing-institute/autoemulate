d33500d04b77dfba23c79122fa969f4296f80ede
mastoffel <martin.adam.stoffel@gmail.com>
2024-03-14 08:08:02 +0000

    add epidemic simulation

--- autoemulate/simulations/epidemic.py
+++ autoemulate/simulations/epidemic.py
@@ -2,36 +2,45 @@ import numpy as np
 from scipy.integrate import solve_ivp
 
 
-def sir_model(t, y, N, beta, gamma):
-    S, I, R = y
-    dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
-    dRdt = gamma * I
-    return [dSdt, dIdt, dRdt]
+def simulate_epidemic(x, N=1000, I0=1):
+    """Simulate an epidemic using the SIR model.
 
+    Parameters
+    ----------
+    x : array-like
+        The parameters of the SIR model. The first element is the transmission rate (beta) and the second element is the recovery rate (gamma).
+    N : int, optional
+        The total population size.
+    I0 : int, optional
+        The initial number of infected individuals.
 
-def simulate_epidemic(X):
+    Returns
+    -------
+    peak_infection_rate : float
+        The peak infection rate as a fraction of the total population.
     """
-    A simplified SIR epidemic model.
+    beta = x[0]
+    gamma = x[1]
 
-    Parameters:
-    N (int): Total population.
-    I0 (int): Initial number of infected individuals.
-    beta (float): Transmission rate.
-    gamma (float): Recovery rate.
-
-    Returns:
-    Peak infection rate (float): Maximum proportion of the population infected at any time.
-    """
-    N, I0, beta, gamma = X[0], X[1], X[2], X[3]
     S0 = N - I0
     R0 = 0
-    t_span = [0, 160]  # Time span for the simulation in days
-    y0 = [S0, I0, R0]  # Initial conditions
+    t_span = [0, 160]
+    y0 = [S0, I0, R0]
 
-    sol = solve_ivp(sir_model, t_span, y0, args=(N, beta, gamma), dense_output=True)
+    def sir_model(t, y, N, beta, gamma):
+        S, I, R = y
+        dSdt = -beta * S * I / N
+        dIdt = beta * S * I / N - gamma * I
+        dRdt = gamma * I
+        return [dSdt, dIdt, dRdt]
 
-    I_max = max(sol.sol(t_span)[1])
+    t_eval = np.linspace(
+        t_span[0], t_span[1], 160
+    )  # Evaluate each day within the time span
+    sol = solve_ivp(sir_model, t_span, y0, args=(N, beta, gamma), t_eval=t_eval)
+
+    S, I, R = sol.y
+    I_max = np.max(I)
     peak_infection_rate = I_max / N
 
     return peak_infection_rate
