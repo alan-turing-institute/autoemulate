37367c706c13264df1bf4ef7bd7ab56507e5dc3e
mastoffel <martin.adam.stoffel@gmail.com>
2024-07-08 10:36:57 +0100

    add CNPDataset class

--- autoemulate/emulators/neural_networks/datasets.py
+++ autoemulate/emulators/neural_networks/datasets.py
@@ -4,28 +4,35 @@ from torch.utils.data import Dataset
 
 
 class CNPDataset(Dataset):
-    def __init__(self, X, y, n_context_points=10):
+    def __init__(self, X, y, max_context_points, min_context_points=3):
         self.X = torch.tensor(X, dtype=torch.float32)
         self.y = torch.tensor(y, dtype=torch.float32)
-        self.n_context_points = n_context_points
+        self.max_context_points = max_context_points
+        self.min_context_points = min_context_points
+        self.n_samples, self.x_dim = X.shape
+        self.y_dim = y.shape[1] if len(y.shape) > 1 else 1
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
-        # select all points
-        X = self.X[idx]
-        y = self.y[idx]
-        # print(f"X: {X.shape}, y: {y.shape}")
+        # sample context points
+        num_context = np.random.randint(
+            self.min_context_points, self.max_context_points
+        )
 
-        # Randomly select context points
-        context_idxs = torch.randperm(len(X))[: self.n_context_points]
-        X_context = X[context_idxs]
-        y_context = y[context_idxs]
+        # randomly select context
+        perm = torch.randperm(self.n_samples)
+        context_idxs = perm[:num_context]
+        target_idxs = (
+            perm  # including context points in targets seems to help with training
+        )
 
-        return {
-            "X_context": X_context,
-            "y_context": y_context,
-            "X_target": X,
-            "y_target": y,
-        }
+        X_context = self.X[context_idxs]
+        y_context = self.y[context_idxs]
+        X_target = self.X[target_idxs]
+        y = self.y[target_idxs]
+
+        X = {"X_context": X_context, "y_context": y_context, "X_target": X_target}
+        y = y
+        return (X, y)
