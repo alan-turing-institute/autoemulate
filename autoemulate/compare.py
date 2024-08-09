8228cf5da152e331b21cedb75c11346dcf78d235
Maximilian Balmus <balmus.maximilian@gmail.com>
2024-08-09 10:55:43 +0100

    Autoemulate.compare: proposed fix for issue #222

--- autoemulate/compare.py
+++ autoemulate/compare.py
@@ -224,17 +224,28 @@ class AutoEmulate:
                                 n_jobs=self.n_jobs,
                                 logger=self.logger,
                             )
+                             # run cross validation with model that uses the optimized hyperparameters
+                            fitted_model, cv_results = _run_cv(
+                                X=self.X[self.train_idxs],
+                                y=self.y[self.train_idxs],
+                                cv=self.cross_validator,
+                                model=self.models[i],
+                                metrics=self.metrics,
+                                n_jobs=self.n_jobs,
+                                logger=self.logger,
+                            )
+                        else:
 
-                        # run cross validation
-                        fitted_model, cv_results = _run_cv(
-                            X=self.X[self.train_idxs],
-                            y=self.y[self.train_idxs],
-                            cv=self.cross_validator,
-                            model=model,
-                            metrics=self.metrics,
-                            n_jobs=self.n_jobs,
-                            logger=self.logger,
-                        )
+                            # run cross validation
+                            fitted_model, cv_results = _run_cv(
+                                X=self.X[self.train_idxs],
+                                y=self.y[self.train_idxs],
+                                cv=self.cross_validator,
+                                model=model,
+                                metrics=self.metrics,
+                                n_jobs=self.n_jobs,
+                                logger=self.logger,
+                            )
                     except Exception:
                         self.logger.exception(
                             f"Error cross-validating model {model_name}"
