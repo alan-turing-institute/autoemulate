57392d65cce58cae86bab3bec2e7a887ec9ec959
radka-j <r.jersakova@gmail.com>
2025-04-03 16:12:43 +0100

    update test

--- tests/experimental/test_experimental_model_selection.py
+++ tests/experimental/test_experimental_model_selection.py
@@ -1,11 +1,14 @@
 import torch
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, LeaveOneOut
 from torch.utils.data import TensorDataset
 
 from autoemulate.experimental.model_selection import cross_validate
 
 
 def test_cross_validate():
+    """
+    Test cross_validate can be called with any sklearn.model_selection class.
+    """
 
     class SimpleEmulator:
         def fit(self, train_loader):
@@ -16,9 +19,15 @@ def test_cross_validate():
 
     x = torch.tensor([1, 2, 3, 4, 5])
     y = 2 * x
-    results = cross_validate(KFold(n_splits=2), TensorDataset(x, y), SimpleEmulator())
 
+    # KFold
+    results = cross_validate(KFold(n_splits=2), TensorDataset(x, y), SimpleEmulator())
     assert "r2" in results
     assert "rmse" in results
     assert len(results["r2"]) == 2
     assert len(results["rmse"]) == 2
+
+    # LOO
+    results = cross_validate(LeaveOneOut(), TensorDataset(x, y), SimpleEmulator())
+    assert len(results["r2"]) == 5
+    assert len(results["rmse"]) == 5
