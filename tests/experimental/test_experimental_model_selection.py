678f71c38345693d18e97a8b150968c285cbe6d0
radka-j <r.jersakova@gmail.com>
2025-04-04 09:04:49 +0100

    clean up test

--- tests/experimental/test_experimental_model_selection.py
+++ tests/experimental/test_experimental_model_selection.py
@@ -24,15 +24,18 @@ def test_cross_validate():
 
     x = torch.tensor([1, 2, 3, 4, 5])
     y = 2 * x
+    dataset = TensorDataset(x, y)
+
+    emulator = DummyEmulator()
 
     # KFold
-    results = cross_validate(KFold(n_splits=2), TensorDataset(x, y), SimpleEmulator())
+    results = cross_validate(KFold(n_splits=2), dataset, emulator)
     assert "r2" in results
     assert "rmse" in results
     assert len(results["r2"]) == 2
     assert len(results["rmse"]) == 2
 
     # LOO
-    results = cross_validate(LeaveOneOut(), TensorDataset(x, y), SimpleEmulator())
+    results = cross_validate(LeaveOneOut(), dataset, emulator)
     assert len(results["r2"]) == 5
     assert len(results["rmse"]) == 5
