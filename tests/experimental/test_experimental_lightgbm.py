d7ac488bffc8edced4a6446992d04080176966df
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-04-15 11:33:18 +0100

    add test_predict_lightgbm

--- tests/experimental/test_experimental_lightgbm.py
+++ tests/experimental/test_experimental_lightgbm.py
@@ -4,6 +4,7 @@ from autoemulate.experimental.emulators.lightgbm.lightgbm import (
     LightGBM,
 )
 from autoemulate.experimental.tuner import Tuner
+from autoemulate.experimental.types import DistributionLike, OutputLike
 from sklearn.datasets import make_regression
 
 
@@ -13,7 +14,22 @@ def sample_data_y1d():
     return torch.Tensor(x), torch.Tensor(y)
 
 
-# def test_predict_lightgbm()
+@pytest.fixture
+def new_data_y1d():
+    x, y = make_regression(n_samples=20, n_features=5, n_targets=1, random_state=1)  # type: ignore
+    return torch.Tensor(x), torch.Tensor(y)
+
+
+def test_predict_lightgbm(sample_data_y1d, new_data_y1d):
+    x, y = sample_data_y1d
+    lgbm = LightGBM(
+        x,
+        y,
+    )
+    lgbm.fit(x, y)
+    x2, _ = new_data_y1d
+    y_pred = lgbm.predict(x2)
+    assert isinstance(y_pred, OutputLike)
 
 
 def test_tune_lightgbm(sample_data_y1d):
