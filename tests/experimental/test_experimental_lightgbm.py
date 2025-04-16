0f8fb5155b4cf8ddb93b1780f62c4c2a4ff6d1f0
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-04-16 09:32:59 +0100

    remove sample data now in conftest.py

--- tests/experimental/test_experimental_lightgbm.py
+++ tests/experimental/test_experimental_lightgbm.py
@@ -8,18 +8,6 @@ from autoemulate.experimental.types import DistributionLike, OutputLike
 from sklearn.datasets import make_regression
 
 
-@pytest.fixture
-def sample_data_y1d():
-    x, y = make_regression(n_samples=20, n_features=5, n_targets=1, random_state=0)  # type: ignore
-    return torch.Tensor(x), torch.Tensor(y)
-
-
-@pytest.fixture
-def new_data_y1d():
-    x, y = make_regression(n_samples=20, n_features=5, n_targets=1, random_state=1)  # type: ignore
-    return torch.Tensor(x), torch.Tensor(y)
-
-
 def test_predict_lightgbm(sample_data_y1d, new_data_y1d):
     x, y = sample_data_y1d
     lgbm = LightGBM(
