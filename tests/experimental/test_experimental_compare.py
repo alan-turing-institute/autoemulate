6e43494cb91953956e72207cf2a65cb71030fc60
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-04-24 16:48:05 +0100

    Add compare test for LightGBM

--- tests/experimental/test_experimental_compare.py
+++ tests/experimental/test_experimental_compare.py
@@ -1,8 +1,21 @@
 from autoemulate.experimental.compare import AutoEmulate
+from autoemulate.experimental.emulators.gaussian_process.exact import (
+    GaussianProcessExact,
+)
+from autoemulate.experimental.emulators.lightgbm import LightGBM
 
 
 def test_compare(sample_data_y2d):
     x, y = sample_data_y2d
-    ae = AutoEmulate(x, y)
+    ae = AutoEmulate(x, y, models=[GaussianProcessExact])
+    results = ae.compare(10)
+    print(results)
+
+
+def test_compare_y1d(sample_data_y1d):
+    x, y = sample_data_y1d
+    # TODO: add handling when 1D
+    y = y.reshape(-1, 1)
+    ae = AutoEmulate(x, y, models=[GaussianProcessExact, LightGBM])
     results = ae.compare(10)
     print(results)
