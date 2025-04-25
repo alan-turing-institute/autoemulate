60bd7c72c27e16c80de8a79f43982a35005dbd20
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-04-25 16:19:05 +0100

    Add CNPModule to ALL_EMULATORS
      Co-authored-by: Radka Jersakova <r.jersakova@gmail.com>

--- autoemulate/experimental/emulators/__init__.py
+++ autoemulate/experimental/emulators/__init__.py
@@ -1,4 +1,5 @@
 from .gaussian_process.exact import GaussianProcessExact
 from .lightgbm import LightGBM
+from .neural_processes.conditional_neural_process import CNPModule
 
-ALL_EMULATORS = [GaussianProcessExact, LightGBM]
+ALL_EMULATORS = [GaussianProcessExact, LightGBM, CNPModule]
