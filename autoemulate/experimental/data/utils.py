450aa48a2f54da038bcd2385bf4fb60b5627e2cb
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-05-19 10:27:32 +0100

    remove todo

--- autoemulate/experimental/data/utils.py
+++ autoemulate/experimental/data/utils.py
@@ -12,7 +12,6 @@ class ConversionMixin:
     Mixin class to convert input data to pytorch Datasets and DataLoaders.
     """
 
-    # TODO: is the distinction between TensorDataset and Dataset important?
     def _convert_to_dataset(
         self,
         x: InputLike,
