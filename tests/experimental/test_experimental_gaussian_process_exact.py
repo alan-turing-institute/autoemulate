fabcd62d5de5c2ded5ce60ca944b6f87b6d57b2f
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-05-21 14:43:28 +0100

    add test_fit_predict_deterministic_with_seed which always passes but perhaps should not

--- tests/experimental/test_experimental_gaussian_process_exact.py
+++ tests/experimental/test_experimental_gaussian_process_exact.py
@@ -1,4 +1,5 @@
 import gpytorch
+import torch
 from autoemulate.emulators.gaussian_process import constant_mean, rbf, rbf_times_linear
 from autoemulate.experimental.emulators.gaussian_process.exact import (
     GaussianProcessExact,
@@ -46,3 +47,34 @@ def test_tune_gp(sample_data_y1d):
     scores, configs = tuner.run(GaussianProcessExact)
     assert len(scores) == 5
     assert len(configs) == 5
+
+
+def test_fit_predict_deterministic_with_seed(sample_data_y1d, new_data_y1d):
+    x, y = sample_data_y1d
+    x2, _ = new_data_y1d
+    # TODO: investigate why this test passes even when
+    # random_state unset or set differently
+    model1 = GaussianProcessExact(
+        x,
+        y,
+        gpytorch.likelihoods.MultitaskGaussianLikelihood,
+        constant_mean,
+        rbf,
+        # random_state=124,
+    )
+    model2 = GaussianProcessExact(
+        x,
+        y,
+        gpytorch.likelihoods.MultitaskGaussianLikelihood,
+        constant_mean,
+        rbf,
+        # random_state=123,
+    )
+    model1.fit(x, y)
+    model2.fit(x, y)
+    pred1 = model1.predict(x2)
+    pred2 = model2.predict(x2)
+    assert isinstance(pred1, DistributionLike)
+    assert isinstance(pred2, DistributionLike)
+    assert torch.allclose(pred1.mean, pred2.mean)
+    assert torch.allclose(pred1.variance, pred2.variance)
