37f0906f05e460d073677ac987d6b104ca1e4e5b
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-05-23 07:36:14 +0100

    Add spectral approach, generalize test

--- autoemulate/experimental/transforms/utils.py
+++ autoemulate/experimental/transforms/utils.py
@@ -2,6 +2,7 @@ import torch
 
 
 def make_positive_definite(cov, epsilon=1e-6, max_tries=3):
+    # Attempt with epsilon first
     for _ in range(max_tries):
         try:
             torch.linalg.cholesky(cov)
@@ -10,6 +11,22 @@ def make_positive_definite(cov, epsilon=1e-6, max_tries=3):
             cov = cov + epsilon * torch.eye(
                 cov.shape[0], device=cov.device, dtype=cov.dtype
             )
+            # Symmetrize?
+            # cov = (cov + cov.T) / 2
+            epsilon *= 10
+
+    # Spectral approach max eigenvalue
+    epsilon = 1e-6
+    for _ in range(max_tries):
+        try:
+            torch.linalg.cholesky(cov)
+            return cov
+        except RuntimeError:
+            eigvals, eigvecs = torch.linalg.eigh(
+                cov, device=cov.device, dtype=cov.dtype
+            )
+            eigvals = torch.clamp(eigvals, min=epsilon)
+            cov = eigvecs @ torch.diag(eigvals) @ eigvecs.T
             epsilon *= 10
     msg = "Matrix could not be made positive definite."
     raise RuntimeError(msg)
--- tests/experimental/test_experimental_transformed.py
+++ tests/experimental/test_experimental_transformed.py
@@ -1,7 +1,7 @@
 import itertools
 
 import pytest
-from autoemulate.experimental.emulators import CNPModule, GaussianProcessExact, LightGBM
+from autoemulate.experimental.emulators import GaussianProcessExact
 from autoemulate.experimental.emulators.transformed.base import TransformedEmulator
 from autoemulate.experimental.transforms import PCATransform, VAETransform
 
@@ -10,27 +10,29 @@ from autoemulate.experimental.types import DistributionLike
 
 
 @pytest.mark.parametrize(
-    ("model", "transform", "target_transform"),
+    ("model", "transform", "target_transforms"),
     itertools.product(
         # [GaussianProcessExact, CNPModule, LightGBM],
-        # [GaussianProcessExact],
         [GaussianProcessExact],
-        # [PCATransform(n_components=3), VAETransform(latent_dim=1)],
-        # [PCATransform(n_components=1), VAETransform(latent_dim=1)],
-        [PCATransform(n_components=3)],
-        [PCATransform(n_components=1)],
+        [
+            None,
+            [PCATransform(n_components=3)],
+            [VAETransform(latent_dim=3)],
+            [PCATransform(n_components=3), VAETransform(latent_dim=2)],
+        ],
+        [None, [PCATransform(n_components=1)], [VAETransform(latent_dim=1)]],
     ),
 )
-def test_transformed_gp_pca(
-    sample_data_y2d, new_data_y2d, model, transform, target_transform
+def test_transformed_emulator(
+    sample_data_y2d, new_data_y2d, model, transform, target_transforms
 ):
     x, y = sample_data_y2d
     x2, _ = new_data_y2d
     em = TransformedEmulator(
         x,
         y,
-        transforms=[transform],
-        target_transforms=[target_transform],
+        transforms=transform,
+        target_transforms=target_transforms,
         model=model,
     )
     em.fit(x, y)
