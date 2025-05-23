6c4b8b34ffb044679c30c79657daf431bc1f5c6b
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-05-23 09:25:29 +0100

    Add test for inverse for gaussians

--- tests/experimental/transforms/test_transforms.py
+++ tests/experimental/transforms/test_transforms.py
@@ -1,4 +1,5 @@
 import pytest
+from autoemulate.experimental.emulators import GaussianProcessExact
 from autoemulate.experimental.transforms import PCATransform, VAETransform
 
 
@@ -16,4 +17,12 @@ def test_transform_shapes(sample_data_y2d, transform):
 @pytest.mark.parametrize(
     ("transform"), [PCATransform(n_components=2), VAETransform(latent_dim=2)]
 )
-def test_transform_sample(sample_data_y2d, transform): ...
+def test_transform_inverse_for_gaussians(sample_data_y2d, transform):
+    x, y = sample_data_y2d
+    transform.fit(y)
+    z = transform(y)
+    gp = GaussianProcessExact(x, z)
+    z_pred = gp.predict(x[: x.shape[0] // 2])
+    for method in [transform._inverse_sample, transform._inverse_gaussian]:
+        y_pred = method(z_pred)
+        assert y_pred.mean.shape == (10, 2)
