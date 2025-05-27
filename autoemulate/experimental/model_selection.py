0f3311c60b56f0a0cdb17c846533d89f07166bc8
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-05-27 12:09:41 +0100

    Add random seed initialization for model fitting in cross-validation

--- autoemulate/experimental/model_selection.py
+++ autoemulate/experimental/model_selection.py
@@ -98,7 +98,8 @@ def cross_validate(
         # fit model
         x, y = next(iter(train_loader))
         # TODO: should we set random_state in the models?
-        # m = model(x, y, random_state=123, **best_model_config)
+        rs = np.random.randint(int(1e5))
+        m = model(x, y, random_state=rs, **best_model_config)
         m = model(x, y, **best_model_config)
         m.fit(x, y)
 
