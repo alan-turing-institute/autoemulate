e40da067920cd017efe257aa2f3e687fbc369275
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-06-03 13:13:09 +0100

    Initial standardize transform

--- autoemulate/experimental/transforms/standardize.py
+++ autoemulate/experimental/transforms/standardize.py
@@ -1,25 +1,25 @@
-import torch
-
 # from torch.distributions import Transform, TransformedDistribution, constraints
-from torch.distributions import Transform, constraints
+import torch
+from torch.distributions import Transform
 
+# from torch.distributions import Transform
 from autoemulate.experimental.transforms.base import AutoEmulateTransform
-
-# from autoemulate.experimental.transforms.utils import make_positive_definite
+from autoemulate.experimental.transforms.utils import make_positive_definite
 from autoemulate.experimental.types import GaussianLike, TensorLike
 
 
 class StandardizeTransform(AutoEmulateTransform):
-    """
-    Standardize transform for normalizing data.
+    """Standardize transform for normalizing data.
+
+    This transform is effectively a composition of two AffineTransforms with a
+    translation by the mean and a scaling by the inverse of the standard deviation.
     """
 
-    domain = constraints.real
-    codomain = constraints.real
     bijective = True
 
-    def __init__(self, cache_size: int = 0):
+    def __init__(self, event_dim=0, cache_size=0):
         Transform.__init__(self, cache_size=cache_size)
+        self._event_dim = event_dim
 
     def fit(self, x: TensorLike):
         self.mean = x.mean(0, keepdim=True)
@@ -37,21 +37,56 @@ class StandardizeTransform(AutoEmulateTransform):
     def log_abs_det_jacobian(self, x, y):
         _, _ = x, y
         self._check_is_fitted()
-        return -torch.log(self.std).sum()
+        return torch.abs(self.std).log().sum(dim=self._event_dim, keepdim=True)
 
     def _inverse_gaussian(self, x: GaussianLike) -> GaussianLike:
-        _ = x
         self._check_is_fitted()
-        # TODO: complete impl
-        msg = "TODO"
-        raise NotImplementedError(msg)
-        # mean_z = x.mean
-        # cov_z = x.covariance_matrix
-        # mean_orig = x.mean + self.mean
-        # assert isinstance(cov_z, TensorLike)
-        # cov_orig = x.covariance_matrix * torch.diag(self.std.view(-1, 1))
-        # return TransformedDistribution(x, [self])
+        mean = x.mean
+        cov = x.covariance_matrix
+        assert isinstance(mean, TensorLike)
+        assert isinstance(cov, TensorLike)
+
+        components = torch.eye(self.std.shape[1]) * self.std
+
+        # Expand components to match covariance matrix size across samples and tasks
+        components_expanded = torch.kron(torch.eye(mean.shape[0]), components)
+
+        # Transform covariance
+        cov_orig = components_expanded @ cov @ components_expanded.T
+        cov_orig = make_positive_definite(cov_orig)
+
+        # Transform mean
+        mean_orig = mean @ components.T  # (n, n_c) x (n_c, d)
+
+        return GaussianLike(mean_orig, cov_orig)
 
     def _inverse_sample(self, x: GaussianLike, n_samples: int = 100) -> GaussianLike:
-        msg = "Inverse sampling not implemented for StandardizeTransform."
-        raise NotImplementedError(msg)
+        # return self._inverse_gaussian(x)
+        self._check_is_fitted()
+        mean = x.mean
+        cov = x.covariance_matrix
+        components = torch.eye(self.std.shape[1]) * self.std
+
+        # TODO: refactor below into a utility function
+        mean_orig = mean @ components.T
+        assert isinstance(mean, TensorLike)
+        assert isinstance(cov, TensorLike)
+
+        def sample_cov():
+            sample = x.sample()
+            sample_orig = (sample @ components.T + mean_orig).view(-1, 1)
+            mean_reshaped = mean_orig.view(-1, 1)
+            return (
+                (sample_orig - mean_reshaped)
+                @ (sample_orig - mean_reshaped).T
+                / (sample_orig.shape[0] - 1)
+            )
+
+        # Generate samples and take the mean to estimate covariance in original space
+        cov_orig = torch.stack([sample_cov() for _ in range(n_samples)]).mean(0)
+
+        print(cov_orig.round(), mean_orig.shape)
+        # Ensure positive definiteness
+        cov_orig = make_positive_definite(cov_orig)
+
+        return GaussianLike(mean_orig, cov_orig)
--- tests/experimental/transforms/test_transforms.py
+++ tests/experimental/transforms/test_transforms.py
@@ -1,27 +1,35 @@
 import pytest
 from autoemulate.experimental.emulators import GaussianProcessExact
 from autoemulate.experimental.transforms import PCATransform, VAETransform
+from autoemulate.experimental.transforms.standardize import StandardizeTransform
 
 
 @pytest.mark.parametrize(
-    ("transform"), [PCATransform(n_components=2), VAETransform(latent_dim=2)]
+    ("transform", "expected_shape"),
+    [
+        (PCATransform(n_components=2), (20, 2)),
+        (VAETransform(latent_dim=2), (20, 2)),
+        (StandardizeTransform(), (20, 5)),
+    ],
 )
-def test_transform_shapes(sample_data_y2d, transform):
+def test_transform_shapes(sample_data_y2d, transform, expected_shape):
     x, _ = sample_data_y2d
     transform.fit(x)
     z = transform(x)
-    assert z.shape == (20, 2)
+    assert z.shape == expected_shape
     assert transform.inv(z).shape == (20, 5)
 
 
 @pytest.mark.parametrize(
-    ("transform"), [PCATransform(n_components=2), VAETransform(latent_dim=2)]
+    ("transform"),
+    [PCATransform(n_components=2), VAETransform(latent_dim=2), StandardizeTransform()],
 )
 def test_transform_inverse_for_gaussians(sample_data_y2d, transform):
     x, y = sample_data_y2d
     transform.fit(y)
     z = transform(y)
     gp = GaussianProcessExact(x, z)
+    gp.fit(x, z)
     z_pred = gp.predict(x[: x.shape[0] // 2])
     for method in [transform._inverse_sample, transform._inverse_gaussian]:
         y_pred = method(z_pred)
