f95abf58fa57a082f53fc92d195edc3af10f6ba4
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-06-04 15:00:43 +0100

    Remove CNPModule from emulators

--- autoemulate/experimental/emulators/__init__.py
+++ autoemulate/experimental/emulators/__init__.py
@@ -1,13 +1,14 @@
 from .gaussian_process.exact import GaussianProcessExact
 from .lightgbm import LightGBM
-from .neural_processes.conditional_neural_process import CNPModule
+
+# from .neural_processes.conditional_neural_process import CNPModule
 from .random_forest import RandomForest
 from .svm import SupportVectorMachine
 
 ALL_EMULATORS = [
     GaussianProcessExact,
     LightGBM,
-    CNPModule,
+    # CNPModule,
     SupportVectorMachine,
     RandomForest,
 ]
