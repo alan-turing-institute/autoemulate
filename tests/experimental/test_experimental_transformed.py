09d7151d64004b9acd8e83ccae2faf591621c1b3
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-06-04 17:42:45 +0100

    Add tests for large number of targets

--- tests/experimental/test_experimental_transformed.py
+++ tests/experimental/test_experimental_transformed.py
@@ -13,6 +13,26 @@ from autoemulate.experimental.transforms import (
 from autoemulate.experimental.types import DistributionLike, TensorLike
 
 
+def run_test(train_data, test_data, model, transform, target_transforms):
+    x, y = train_data
+    x2, _ = test_data
+    em = TransformedEmulator(
+        x,
+        y,
+        transforms=transform,
+        target_transforms=target_transforms,
+        model=model,
+    )
+    em.fit(x, y)
+    y_pred = em.predict(x2)
+    if model is GaussianProcessExact:
+        assert isinstance(y_pred, DistributionLike)
+        assert y_pred.mean.shape == (x2.shape[0], y.shape[1])
+    else:
+        assert isinstance(y_pred, TensorLike)
+        assert y_pred.shape == (x2.shape[0], y.shape[1])
+
+
 @pytest.mark.parametrize(
     ("model", "transform", "target_transforms"),
     itertools.product(
@@ -38,23 +58,55 @@ from autoemulate.experimental.types import DistributionLike, TensorLike
 def test_transformed_emulator(
     sample_data_y2d, new_data_y2d, model, transform, target_transforms
 ):
-    x, y = sample_data_y2d
-    x2, _ = new_data_y2d
-    em = TransformedEmulator(
-        x,
-        y,
-        transforms=transform,
-        target_transforms=target_transforms,
-        model=model,
+    run_test(sample_data_y2d, new_data_y2d, model, transform, target_transforms)
+
+
+@pytest.mark.parametrize(
+    ("model", "transform", "target_transforms"),
+    itertools.product(
+        [emulator for emulator in ALL_EMULATORS if emulator.is_multioutput()],
+        [
+            None,
+            [PCATransform(n_components=3)],
+            [VAETransform(latent_dim=3)],
+            [
+                StandardizeTransform(),
+                PCATransform(n_components=3),
+                VAETransform(latent_dim=2),
+            ],
+        ],
+        [
+            # TODO: check error when no target transforms are provided
+            # None,
+            [PCATransform(n_components=10)],
+            [PCATransform(n_components=10)],
+            [PCATransform(n_components=15)],
+            # TODO: check error for n_components = 20
+            # ValueError: Input tensor y contains non-finite values
+            # [PCATransform(n_components=20)],
+            [VAETransform(latent_dim=10)],
+            # TODO: check error for latent_dim = 20
+            # ValueError: Input tensor y contains non-finite values
+            # [VAETransform(latent_dim=20)],
+            [StandardizeTransform()],
+            [StandardizeTransform(), VAETransform(latent_dim=10)],
+        ],
+    ),
+)
+def test_transformed_emulator_100_targets(
+    sample_data_y2d_100_targets,
+    new_data_y2d_100_targets,
+    model,
+    transform,
+    target_transforms,
+):
+    run_test(
+        sample_data_y2d_100_targets,
+        new_data_y2d_100_targets,
+        model,
+        transform,
+        target_transforms,
     )
-    em.fit(x, y)
-    y_pred = em.predict(x2)
-    if model is GaussianProcessExact:
-        assert isinstance(y_pred, DistributionLike)
-        assert y_pred.mean.shape == (20, 2)
-    else:
-        assert isinstance(y_pred, TensorLike)
-        assert y_pred.shape == (20, 2)
 
 
 # def test_tune_transformed_gp(sample_data_y2d):
