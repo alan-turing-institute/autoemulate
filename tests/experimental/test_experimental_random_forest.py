601e7090cefd7d405f958dc55270632cece09c63
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-04 11:31:00 +0100

    add determinism test for rf

--- tests/experimental/test_experimental_random_forest.py
+++ tests/experimental/test_experimental_random_forest.py
@@ -1,3 +1,4 @@
+import torch
 from autoemulate.experimental.emulators.random_forest import (
     RandomForest,
 )
@@ -30,3 +31,17 @@ def test_tune_rf(sample_data_y1d):
     scores, configs = tuner.run(RandomForest)
     assert len(scores) == 5
     assert len(configs) == 5
+
+
+def test_fit_predict_deterministic_with_seed(sample_data_y1d, new_data_y1d):
+    x, y = sample_data_y1d
+    x2, _ = new_data_y1d
+    model1 = RandomForest(x, y, random_seed=42)
+    model2 = RandomForest(x, y, random_seed=42)
+    model1.fit(x, y)
+    model2.fit(x, y)
+    pred1 = model1.predict(x2)
+    pred2 = model2.predict(x2)
+    assert isinstance(pred1, TensorLike)
+    assert isinstance(pred2, TensorLike)
+    assert torch.allclose(pred1, pred2)
