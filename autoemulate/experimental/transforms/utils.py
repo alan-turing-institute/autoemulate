dd770ed44ea27cc7323078e746c63610264e69e4
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-06-06 16:07:40 +0100

    Revise function defaults, add warnings and docstring

--- autoemulate/experimental/transforms/utils.py
+++ autoemulate/experimental/transforms/utils.py
@@ -1,16 +1,48 @@
+import warnings
+
 import torch
+from linear_operator.utils.warnings import NumericalWarning
 
 
-# TODO: consider if this function is sufficiently robust for all cases.
-# TODO: compare with linear_operator's psd_safe_cholesky:
-# `from linear_operator.utils.cholesky import psd_safe_cholesky``
 def make_positive_definite(
-    cov, epsilon=1e-6, min_eigval=1e-6, max_tries_epsilon=3, max_tries_min_eigval=5
+    cov, epsilon=1e-6, min_eigval=1e-6, max_tries_epsilon=3, max_tries_min_eigval=1
 ):
+    """Ensure a covariance matrix is positive definite by:
+        1. adding `epsilon` to the diagonal and symmetrizing the matrix
+        2. if this fails, clamping eigenvalues to a minimum value `min_eigval`
+
+    See related function in linear_operator: `psd_safe_cholesky`.
+
+    Parameters
+    ----------
+    cov (torch.Tensor): The covariance matrix to be made positive definite.
+    epsilon (float): Initial value to add to the diagonal for numerical stability.
+        Default is 1e-6.
+    min_eigval (float): Minimum eigenvalue to clamp to if the matrix is not positive
+        definite.
+        Default is 1e-6.
+    max_tries_epsilon (int): Maximum number of attempts to add `epsilon` to the
+        diagonal.
+        Default is 3.
+    max_tries_min_eigval (int): Maximum number of attempts to clamp eigenvalues.
+        Default is 1.
+
+    Returns
+    -------
+    torch.Tensor: A positive definite covariance matrix.
+
+    """
     # Attempt with epsilon first
-    for _ in range(max_tries_epsilon):
+    for i in range(max_tries_epsilon):
         try:
             torch.linalg.cholesky(cov)
+            if i > 0:
+                warnings.warn(
+                    f"cov not p.d. - added {epsilon:.1e} to the diagonal and "
+                    "symmetrized",
+                    NumericalWarning,
+                    stacklevel=2,
+                )
             return cov
         except RuntimeError:
             cov = cov + epsilon * torch.eye(
@@ -21,9 +53,16 @@ def make_positive_definite(
             epsilon *= 10
 
     # Spectral approach by clamping eigenvalues
-    for _ in range(max_tries_min_eigval):
+    for i in range(max_tries_min_eigval):
         try:
             torch.linalg.cholesky(cov)
+            if i > 0:
+                warnings.warn(
+                    f"cov not p.d. - clamped eigval to {min_eigval:.1e} and "
+                    "symmetrized",
+                    NumericalWarning,
+                    stacklevel=2,
+                )
             return cov
         except RuntimeError:
             eigvals, eigvecs = torch.linalg.eigh(cov)
