c7b7f0adefb2bb7f264912ab047a0d0186a084aa
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-24 15:36:16 +0100

    remove y2d test

--- tests/experimental/test_experimental_gradient_boosting.py
+++ tests/experimental/test_experimental_gradient_boosting.py
@@ -14,16 +14,6 @@ def test_predict_gb(sample_data_y1d, new_data_y1d):
     assert isinstance(y_pred, TensorLike)
 
 
-def test_predict_gb_2d(sample_data_y2d, new_data_y2d):
-    x, y = sample_data_y2d
-    gb = GradientBoosting(x, y)
-    gb.fit(x, y)
-    x2, _ = new_data_y2d
-    y_pred = gb.predict(x2)
-    assert isinstance(y_pred, TensorLike)
-    assert y_pred.shape == (20, 2)
-
-
 def test_tune_gb(sample_data_y1d):
     x, y = sample_data_y1d
     tuner = Tuner(x, y, n_iter=5)
