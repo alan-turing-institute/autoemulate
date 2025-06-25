a21a9c89d4b98d71ac2e3abe37593b811e627f51
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-25 09:52:54 +0100

    add radial basis function test fixtures and update tests to use them

--- tests/experimental/test_experimental_radial_basis_functions.py
+++ tests/experimental/test_experimental_radial_basis_functions.py
@@ -5,18 +5,18 @@ from autoemulate.experimental.tuner import Tuner
 from autoemulate.experimental.types import TensorLike
 
 
-def test_predict_rbf(sample_data_y2d, new_data_y2d):
-    x, y = sample_data_y2d
+def test_predict_rbf(sample_data_rbf, new_data_rbf):
+    x, y = sample_data_rbf
     rbf = RadialBasisFunctions(x, y)
     rbf.fit(x, y)
-    x2, _ = new_data_y2d
+    x2, _ = new_data_rbf
     y_pred = rbf.predict(x2)
     assert isinstance(y_pred, TensorLike)
-    assert y_pred.shape == (20, 2)
+    assert y_pred.shape == (56, 2)
 
 
-def test_tune_rbf(sample_data_y2d):
-    x, y = sample_data_y2d
+def test_tune_rbf(sample_data_rbf):
+    x, y = sample_data_rbf
     tuner = Tuner(x, y, n_iter=5)
     scores, configs = tuner.run(RadialBasisFunctions)
     assert len(scores) == 5
