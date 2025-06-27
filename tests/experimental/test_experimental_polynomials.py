b6fcf5601ad5fd9812a05a6c4a2045fe0982c758
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-06-27 16:31:43 +0100

    rename sop to pr

--- tests/experimental/test_experimental_polynomials.py
+++ tests/experimental/test_experimental_polynomials.py
@@ -11,27 +11,27 @@ from autoemulate.experimental.tuner import Tuner
 from autoemulate.experimental.types import TensorLike
 
 
-def test_predict_sop(sample_data_y1d, new_data_y1d):
+def test_predict_pr(sample_data_y1d, new_data_y1d):
     x, y = sample_data_y1d
-    sop = PolynomialRegression(x, y)
-    sop.fit(x, y)
+    pr = PolynomialRegression(x, y)
+    pr.fit(x, y)
     x2, _ = new_data_y1d
-    y_pred = sop.predict(x2)
+    y_pred = pr.predict(x2)
     assert isinstance(y_pred, TensorLike)
 
 
-def test_predict_sop_2d(sample_data_y2d, new_data_y2d):
+def test_predict_pr_2d(sample_data_y2d, new_data_y2d):
     x, y = sample_data_y2d
-    sop = PolynomialRegression(x, y)
-    sop.fit(x, y)
+    pr = PolynomialRegression(x, y)
+    pr.fit(x, y)
     x2, _ = new_data_y2d
-    y_pred = sop.predict(x2)
+    y_pred = pr.predict(x2)
     assert isinstance(y_pred, TensorLike)
     assert y_pred.shape == (20, 2)
 
 
 @pytest.mark.parametrize("device", SUPPORTED_DEVICES)
-def test_tune_sop(sample_data_y1d, device):
+def test_tune_pr(sample_data_y1d, device):
     if not check_torch_device_is_available(device):
         pytest.skip(f"Device ({device}) is not available.")
     x, y = sample_data_y1d
@@ -42,7 +42,7 @@ def test_tune_sop(sample_data_y1d, device):
     assert len(configs) == n_iter
 
 
-def test_sop_predict_deterministic_with_seed(sample_data_y2d, new_data_y2d):
+def test_pr_predict_deterministic_with_seed(sample_data_y2d, new_data_y2d):
     """
     Test that fitting two models with the same seed and data
     produces identical predictions.
