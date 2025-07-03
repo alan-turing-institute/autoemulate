bb71a2ee628af9fa4cb3ef542780ad987471cdec
radka-j <r.jersakova@gmail.com>
2025-07-03 18:16:32 +0100

    add tests

---
+++ tests/experimental/test_experimental_early_stopping.py
@@ -0,0 +1,56 @@
+from unittest.mock import MagicMock
+
+import pytest
+import torch
+from gpytorch.likelihoods import MultitaskGaussianLikelihood
+
+from autoemulate.experimental.emulators.early_stopping import EarlyStopping
+from autoemulate.experimental.emulators.gaussian_process.exact import (
+    GaussianProcessExact,
+)
+
+
+@pytest.fixture
+def gp_exact():
+    x = torch.randn(10, 3)
+    y = torch.randn(10, 2)
+    return GaussianProcessExact(
+        x=x,
+        y=y,
+        likelihood_cls=MultitaskGaussianLikelihood,
+        epochs=5,
+        batch_size=2,
+        lr=0.1,
+        early_stopping=None,
+    )
+
+
+def test_early_stopping_method_calls_counts(gp_exact):
+    """
+    Test that early stopping methods are called correct number of times.
+    """
+    early_stopping = MagicMock(spec=EarlyStopping)
+    gp_exact.early_stopping = early_stopping
+
+    x = torch.randn(10, 3)
+    y = torch.randn(10, 2)
+    gp_exact._fit(x, y)
+
+    early_stopping.on_train_begin.assert_called_once()
+    early_stopping.on_train_end.assert_called_once()
+    assert early_stopping.on_epoch_end.call_count == gp_exact.epochs
+
+
+def test_early_stopping_trigger(gp_exact):
+    """Test that early stopping is triggered."""
+    # set very high threshold and low patience to trigger it
+    early_stopping = EarlyStopping(threshold=100, patience=2)
+    gp_exact.early_stopping = early_stopping
+
+    # Run the training process
+    x = torch.randn(10, 3)
+    y = torch.randn(10, 2)
+    gp_exact._fit(x, y)
+
+    # Verify that early stopping was triggered
+    assert early_stopping.misses_ == early_stopping.patience
