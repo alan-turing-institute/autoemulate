490d7b09711f7647361bec89cc9edcc207638bf3
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-07-08 17:23:48 +0100

    Simplify tuner

--- autoemulate/experimental/tuner.py
+++ autoemulate/experimental/tuner.py
@@ -1,4 +1,3 @@
-import torch
 from sklearn.model_selection import KFold
 from torchmetrics import R2Score
 from tqdm import tqdm
@@ -8,12 +7,7 @@ from autoemulate.experimental.device import TorchDeviceMixin
 from autoemulate.experimental.emulators.base import ConversionMixin, Emulator
 from autoemulate.experimental.model_selection import cross_validate
 from autoemulate.experimental.transforms.base import AutoEmulateTransform
-from autoemulate.experimental.types import (
-    DeviceLike,
-    InputLike,
-    ModelConfig,
-    TensorLike,
-)
+from autoemulate.experimental.types import DeviceLike, InputLike, ModelConfig
 
 
 class Tuner(ConversionMixin, TorchDeviceMixin):
@@ -75,32 +69,16 @@ class Tuner(ConversionMixin, TorchDeviceMixin):
         Tuple[list[float], list[ModelConfig]]
             The validation scores and parameter values used in each search iteration.
         """
-        # split data into train/validation sets
-        # batch size defaults to size of train data if not otherwise specified
-        train_loader, val_loader = self._random_split(self.dataset)
-
-        train_x: TensorLike
-        train_y: TensorLike
-        train_x, train_y = next(iter(train_loader))
-
-        val_x: TensorLike
-        val_y: TensorLike
-        val_x, val_y = next(iter(val_loader))
-
         # keep track of what parameter values tested and how they performed
         model_config_tested: list[ModelConfig] = []
         val_scores: list[float] = []
-
         for _ in tqdm(range(self.n_iter)):
             # randomly sample hyperparameters and instantiate model
             model_config = model_class.get_random_config()
 
             scores = cross_validate(
                 cv=KFold(n_splits=n_splits, random_state=seed, shuffle=shuffle),
-                dataset=ConversionMixin._convert_to_dataset(
-                    torch.cat([train_x, val_x], dim=0),
-                    torch.cat([train_y, val_y], dim=0),
-                ),
+                dataset=self.dataset,
                 x_transforms=x_transforms,
                 y_transforms=y_transforms,
                 model=model_class,
