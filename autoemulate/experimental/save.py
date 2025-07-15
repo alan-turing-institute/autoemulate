5a2687493694b6431b736261a9fc1c096023889a
Ed Chalstrey <edwardchalstrey@gmail.com>
2025-07-15 11:19:01 +0100

    add ModelSerialiser class for saving and loading models

---
+++ autoemulate/experimental/save.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+import joblib
+
+from autoemulate.utils import get_model_name
+
+
+class ModelSerialiser:
+    def __init__(self, logger):
+        self.logger = logger
+
+    def _save_model(self, model, path):
+        """Saves a model to disk.
+
+        Parameters
+        ----------
+        model : scikit-learn model
+            Model to save.
+        path : str, Path or None, optional
+            Path to save the model.
+            If None, the model will be saved with the model name.
+        """
+        model_name = self._get_model_name(model)
+        full_path = self._prepare_path(path, model_name)
+
+        try:
+            joblib.dump(model, full_path)
+            self.logger.info("%s saved to %s", model_name, full_path)
+        except Exception as e:
+            self.logger.error("Failed to save %s to %s: %s", model_name, full_path, e)
+            raise
+
+    def _load_model(self, path):
+        """Loads a model from disk.
+
+        Parameters
+        ----------
+        path : str or Path
+            Path to load the model.
+        """
+        path = Path(path)
+        try:
+            model = joblib.load(path)
+            self.logger.info("Model loaded from %s", path)
+            return model
+        except Exception as e:
+            self.logger.error("Failed to load model from %s: %s", path, e)
+            raise
+
+    def _prepare_path(self, path, model_name):
+        """Prepares path for saving model."""
+        if path is None:
+            full_path = Path(model_name)
+        else:
+            full_path = Path(path)
+            if full_path.is_dir():
+                full_path = full_path / model_name
+
+        full_path.parent.mkdir(parents=True, exist_ok=True)
+        return full_path
+
+    @staticmethod
+    def _get_model_name(model):
+        """Gets the name of the model to save."""
+        model_name = get_model_name(model)
+        return f"{model_name}.joblib"
