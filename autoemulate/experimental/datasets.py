09f52f8de15fdf893d257900be789a264daf259c
radka-j <r.jersakova@gmail.com>
2025-07-16 18:12:01 +0100

    handle return type in fetch_data if split=True

--- autoemulate/experimental/datasets.py
+++ autoemulate/experimental/datasets.py
@@ -11,7 +11,7 @@ data_dir = Path(__file__).parent.parent.parent / "data"
 
 def fetch_data(
     dataset: str, split: bool = False, test_size: float = 0.2, random_state: int = 42
-) -> tuple[TensorLike, TensorLike]:
+) -> list[TensorLike]:
     """
     Fetch a dataset by name.
 
@@ -33,26 +33,35 @@ def fetch_data(
                         MAXPMOC, OCN_O2, fCaCO3, SIAREA_S.
         - **engineering1**: Cantilever truss simulation.
         - **reactiondiffusion1**: Reaction-diffusion system simulated data. Spatial
-                        snapshots in correspondence for different values of reaction and
-                        diffusion parameters. (50 snapshots in corresponence of 50
-                        different parameter sets at time T=10).
+                        snapshots in correspondence for different values of reaction
+                        and  ddiffusion parameters. (50 snapshots in correspondence of
+                        50 different parameter sets at time T=10).
     split: bool
         Whether to split the data into training and testing sets. Default is False.
     test_size: float
         The proportion of the dataset to include in the test split. Default is 0.2.
     random_state: int
-        Controls the shuffling applied to the data before applying the split. Default is 42.
+        Controls the shuffling applied to the data before applying the split. Default
+        is 42.
 
     Returns
     -------
-    tuple[TensorLike, TensorLike]
-        Tensors of simulation parameters / inputs and simulation outputs.
+    list[TensorLike]
+        Tensors of simulation parameters / inputs and simulation outputs. If `split`
+        is True, returns [x_train, x_test, y_train, y_test].
     """
     data_dir_dataset = data_dir / dataset / "processed"
     X = pd.read_csv(data_dir_dataset / "parameters.csv").to_numpy()
     Y = pd.read_csv(data_dir_dataset / "outputs.csv").to_numpy()
 
     if split:
-        return train_test_split(X, Y, test_size=test_size, random_state=random_state)
-    else:
-        return torch.tensor(X), torch.tensor(Y)
+        x_train, x_test, y_train, y_test = train_test_split(
+            X, Y, test_size=test_size, random_state=random_state
+        )
+        return [
+            torch.tensor(x_train),
+            torch.tensor(x_test),
+            torch.tensor(y_train),
+            torch.tensor(y_test),
+        ]
+    return [torch.tensor(X), torch.tensor(Y)]
