52e5439c53876d679f20438cb9088127789bb18a
radka-j <r.jersakova@gmail.com>
2025-07-16 15:55:57 +0100

    update docstrings

--- autoemulate/experimental/simulations/experimental_design.py
+++ autoemulate/experimental/simulations/experimental_design.py
@@ -10,20 +10,21 @@ class ExperimentalDesign(ABC):
     """
     Abstract class for experimental design.
 
-    This class is used to sample points from the parameter space
-    to be used in the training of the emulator.
-
-    Attributes
-    ----------
-    bounds_list : list
-        List tuples with two numeric values.
-        Each tuple corresponds to the lower and
-        upper bounds of a parameter.
+    This class is used to sample points from the parameter space to be used in the
+    training of an emulator.
     """
 
     @abstractmethod
     def __init__(self, bounds_list: list[tuple[float, float]]):
-        """Initializes a Sampler object."""
+        """
+        Initializes a Sampler object.
+
+        Parameters
+        ----------
+        bounds_list : list
+            List tuples with two numeric values. Each tuple corresponds to the lower and
+            upper bounds of a parameter.
+        """
 
     @abstractmethod
     def sample(self, n: int) -> TensorLike:
@@ -34,6 +35,11 @@ class ExperimentalDesign(ABC):
         ----------
         n: int
             The number of points to sample.
+
+        Returns
+        -------
+        TensorLike
+            A tensor of shape (n, dim) containing the sampled points.
         """
 
     @abstractmethod
@@ -50,17 +56,7 @@ class ExperimentalDesign(ABC):
 
 class LatinHypercube(ExperimentalDesign):
     """
-    LatinHypercube class for experimental design.
-
-    This class is used to sample points from the parameter space
-    to be used in the training of the emulator.
-
-    Attributes
-    ----------
-    bounds_list: list
-        List tuples with two numeric values.
-        Each tuple corresponds to the lower and
-        upper bounds of a parameter.
+    LatinHypercube experimental design class.
     """
 
     def __init__(self, bounds_list: list[tuple[float, float]]):
@@ -68,28 +64,8 @@ class LatinHypercube(ExperimentalDesign):
         self.sampler = mogp_emulator.LatinHypercubeDesign(bounds_list)
 
     def sample(self, n: int) -> TensorLike:
-        """
-        Samples n points from the sample space.
-
-        Parameters
-        ----------
-        n: int
-            The number of points to sample.
-
-        Returns
-        -------
-        samples : numpy.ndarray
-            A numpy array of shape (n, dim) containing the sampled points.
-        """
         sample_array = self.sampler.sample(n)
         return torch.tensor(sample_array, dtype=torch.float32)
 
     def get_n_parameters(self) -> int:
-        """Returns the number of parameters in the sample space.
-
-        Returns
-        -------
-        int
-            The number of parameters in the sample space.
-        """
         return self.sampler.get_n_parameters()
