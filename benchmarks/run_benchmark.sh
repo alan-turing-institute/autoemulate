b8f4922582c20bb66d0306b53362d3996ac6ee61
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-07-24 17:40:00 +0100

    Fix script location

--- benchmarks/run_benchmark.sh
+++ benchmarks/run_benchmark.sh
@@ -13,7 +13,7 @@ for simulator in Epidemic FlowProblem Projectile ProjectileMultioutput; do
       n_iter1=${n_iter_array[0]}
       n_iter2=${n_iter_array[1]}
       echo "Running benchmark for simulator: $simulator, n_splits: $n_splits, n_iter: $n_iter1 $n_iter2"
-      pueue add "python autoemulate/experimental/benchmark.py --simulators \"$simulator\" --n_splits_list \"$n_splits\" --n_iter_list \"$n_iter1\" --n_iter_list \"$n_iter2\" --log_level info --output_file \"${outpath}/benchmark_results_${simulator}_n_splits_${n_splits}_n_iter_${n_iter1}_${n_iter2}.csv\""
+      pueue add "python benchmarks/benchmark.py --simulators \"$simulator\" --n_splits_list \"$n_splits\" --n_iter_list \"$n_iter1\" --n_iter_list \"$n_iter2\" --log_level info --output_file \"${outpath}/benchmark_results_${simulator}_n_splits_${n_splits}_n_iter_${n_iter1}_${n_iter2}.csv\""
     done
   done
 done
