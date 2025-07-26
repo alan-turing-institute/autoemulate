4304a09155539152def1138253b03fda6fca9844
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-07-26 11:34:22 +0100

    Add discrete fourier transform to init

--- autoemulate/transforms/__init__.py
+++ autoemulate/transforms/__init__.py
@@ -1,10 +1,21 @@
+from .discrete_fourier import DiscreteFourierTransform
 from .pca import PCATransform
 from .standardize import StandardizeTransform
 from .vae import VAETransform
 
-__all__ = ["PCATransform", "StandardizeTransform", "VAETransform"]
+__all__ = [
+    "DiscreteFourierTransform",
+    "PCATransform",
+    "StandardizeTransform",
+    "VAETransform",
+]
 
-ALL_TRANSFORMS = [PCATransform, StandardizeTransform, VAETransform]
+ALL_TRANSFORMS = [
+    DiscreteFourierTransform,
+    PCATransform,
+    StandardizeTransform,
+    VAETransform,
+]
 
 # Registry mapping transform names to their classes
 # Add new transforms here when creating them
@@ -12,4 +23,5 @@ TRANSFORM_REGISTRY = {
     "pca": PCATransform,
     "standardize": StandardizeTransform,
     "vae": VAETransform,
+    "discrete_fourier": DiscreteFourierTransform,
 }
