317308568e98777f9337c75a59a5f8e414568beb
Edwin Brown <w.edwin.brown@outlook.com>
2025-08-08 13:56:02 +0100

    fix device test

--- tests/test_device.py
+++ tests/test_device.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 from autoemulate.core.device import (
     SUPPORTED_DEVICES,
+    TURN_OFF_MPS_IF_RUNNING_CI,
     TorchDeviceMixin,
 )
 
@@ -12,8 +13,13 @@ from autoemulate.core.device import (
 def test_device(device):
     fake_xpu = MagicMock()
     fake_xpu.is_available.return_value = True
+
+    # Change the expected MPS availability depending on
+    # whether we are running in a CI environment or not.
+    MPS_AVAILABLE = not TURN_OFF_MPS_IF_RUNNING_CI
+
     with (
-        patch("torch.backends.mps.is_available", return_value=True),
+        patch("torch.backends.mps.is_available", return_value=MPS_AVAILABLE),
         patch("torch.cuda.is_available", return_value=True),
         patch("torch.cuda.device_count", return_value=1),
         patch.object(torch, "xpu", fake_xpu),
