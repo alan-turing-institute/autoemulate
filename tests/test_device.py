29d257d7a63e0344f5a0cc966cdfb256f1c10b5e
Edwin <64434531+EdwinB12@users.noreply.github.com>
2025-08-08 16:40:05 +0100

    681 add testing support for different os (#702)
    * add all os support
    
    * Stop all test jobs failing when one does
    
    * Fix windows path error
    
    * Set pytorch flag to enable mps fallback
    
    * add a comment
    
    * Fix windows path problem
    
    * add high watermark env variable
    
    * change Invalid file path test so it works on windows
    
    * Specifically request arm64 mac runners
    
    * fix runner label
    
    * Force torch to be below 2.8
    
    * attempt to fix memory issues
    
    * Testing whether is built works with CI
    
    * Set env var to turn off mps if running CI
    
    * fix device test
    
    * fix test_device
    
    * change back depeendcy fix
    
    * missing quotation

--- tests/test_device.py
+++ tests/test_device.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 from autoemulate.core.device import (
     SUPPORTED_DEVICES,
+    TURN_OFF_MPS_IF_RUNNING_CI,
     TorchDeviceMixin,
 )
 
@@ -12,8 +13,15 @@ from autoemulate.core.device import (
 def test_device(device):
     fake_xpu = MagicMock()
     fake_xpu.is_available.return_value = True
+
+    # Required to make test work if both being run locally or
+    # run on CI.
+    MPS_ON = not TURN_OFF_MPS_IF_RUNNING_CI
+    if TURN_OFF_MPS_IF_RUNNING_CI and device == "mps":
+        device = "cpu"
+
     with (
-        patch("torch.backends.mps.is_available", return_value=True),
+        patch("torch.backends.mps.is_available", return_value=MPS_ON),
         patch("torch.cuda.is_available", return_value=True),
         patch("torch.cuda.device_count", return_value=1),
         patch.object(torch, "xpu", fake_xpu),
