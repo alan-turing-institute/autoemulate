4ff85714d0a15c262a0ad8cef8c61402b3be1a14
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-09-16 14:31:03 +0100

    Add initial well base class

---
+++ autoemulate/experimental/emulators/the_well.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+from typing import ClassVar
+
+import torch
+import torchinfo
+from autoemulate.core.types import DeviceLike, ModelParams, TensorLike
+from autoemulate.experimental.emulators.spatiotemporal import SpatioTemporalEmulator
+from the_well.benchmark import models
+from the_well.benchmark.metrics import VRMSE
+from the_well.benchmark.trainer import Trainer
+from the_well.data import WellDataModule
+from the_well.data.datamodule import AbstractDataModule
+from torch.utils.data import DataLoader
+
+
+class TheWellEmulator(SpatioTemporalEmulator):
+    """Base class for The Well emulators."""
+
+    model: torch.nn.Module
+    model_cls: type[torch.nn.Module]
+    model_parameters: ClassVar[ModelParams]
+
+    def __init__(
+        self,
+        datamodule: AbstractDataModule | WellDataModule,
+        output_path: str | Path | None,
+        device: DeviceLike = "cpu",
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        # TODO: update path handling
+        output_path = Path(output_path) if output_path else Path("./")
+        checkpoint_path = output_path / "checkpoints"
+        artifact_path = output_path / "artifacts"
+
+        if isinstance(datamodule, WellDataModule):
+            # Load metadata from train_dataset
+            metadata = datamodule.train_dataset.metadata
+            n_steps_input = datamodule.train_dataset.n_steps_input
+            n_steps_output = datamodule.train_dataset.n_steps_output
+            self.model = self.model_cls(
+                **self.model_parameters,
+                # TODO: check if general beyond FNO
+                dim_in=self.n_input_fields,
+                dim_out=self.n_output_fields,
+                n_spatial_dims=metadata.n_spatial_dims,
+                spatial_resolution=metadata.spatial_resolution,
+            )
+            self.n_input_fields = (
+                n_steps_input * metadata.n_fields + metadata.n_constant_fields
+            )
+            self.n_output_fields = metadata.n_fields
+            print(torchinfo.summary(self.model, depth=5))
+        else:
+            msg = "Alternative datamodules not yet supported"
+            raise NotImplementedError(msg)
+
+        device = torch.device("cpu")
+        optimizer: torch.optim.Optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=1e-3
+        )
+        lr_scheduler = None
+
+        self.trainer = Trainer(
+            checkpoint_folder=str(checkpoint_path),
+            artifact_folder=str(artifact_path),
+            viz_folder=str(artifact_path / "viz"),
+            # TODO: make the below configurable
+            formatter="channels_first_default",  # "channels_last_default" for others
+            model=self.model,
+            datamodule=datamodule,
+            optimizer=optimizer,
+            loss_fn=VRMSE(),
+            epochs=10,
+            checkpoint_frequency=5,
+            val_frequency=1,
+            rollout_val_frequency=5,
+            max_rollout_steps=10,
+            short_validation_length=20,
+            make_rollout_videos=False,
+            num_time_intervals=n_steps_output,
+            lr_scheduler=lr_scheduler,
+            device=device,
+            is_distributed=False,
+            enable_amp=False,
+            amp_type="float16",  # bfloat not supported in FFT
+            checkpoint_path="",  # Path to a checkpoint to resume from, if any
+        )
+
+    def fit(
+        self,
+        x: TensorLike | DataLoader | None = None,  # noqa: ARG002
+        y: TensorLike | None = None,  # noqa: ARG002
+        **kwargs,
+    ):
+        """Train a spatio-temporal emulator.
+
+        Parameters
+        ----------
+        x: TensorLike | DataLoader | None
+            Input features as `TensorLike` or `DataLoader`.
+        y: OutputLike | None
+            Target values (not needed if x is a DataLoader).
+        """
+        self.trainer.train()
+
+    def predict(self, x: TensorLike | DataLoader | None, with_grad=False):
+        """Predict using the spatio-temporal emulator."""
+        self.eval()
+        with torch.set_grad_enabled(with_grad):
+            if isinstance(x, DataLoader):
+                dataloader = x
+            else:
+                msg = "x must be a DataLoader"
+                raise ValueError(msg)
+            preds = []
+            for batch in dataloader:
+                preds.append(
+                    self.trainer.rollout_model(
+                        self.model, batch, self.trainer.formatter, train=False
+                    )
+                )
+            return torch.cat(preds, 0)
+
+    def predict_autoregressive(  # noqa: D102 # type: ignore [override] # (n_steps not used) and initial state derived from dataloader
+        self, x: TensorLike | DataLoader | None, with_grad=False
+    ):
+        return self.predict(x, with_grad)
+
+
+class TheWellFNO(TheWellEmulator):
+    """The Well FNO emulator."""
+
+    model_cls: type[torch.nn.Module] = models.FNO
+    model_parameters: ClassVar[ModelParams] = {
+        "modes1": 16,
+        "modes2": 16,
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
