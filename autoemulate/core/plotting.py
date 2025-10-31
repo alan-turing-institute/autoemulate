303fb19b54c06727fb9168aad4a9489a5f3a291b
Radka Jersakova <r.jersakova@gmail.com>
2025-10-31 08:59:09 +0000

    Update autoemulate/core/plotting.py
    Co-authored-by: Sam Greenbury <50113363+sgreenbury@users.noreply.github.com>

--- autoemulate/core/plotting.py
+++ autoemulate/core/plotting.py
@@ -97,7 +97,7 @@ def plot_xy(
     assert ax is not None, "ax must be provided"
     # Scatter plot with error bars for predictions
     if y_std is not None:
-        if error_style not in ["bars", "fill"]:
+        if error_style.lower() not in ["bars", "fill"]:
             msg = "error_style must be one of ['bars', 'fill']"
             raise ValueError(msg)
         if error_style == "bars":
