bb4a4dd6ee9c5f24ee9ec545d36d049e741f6b65
Georgia Seinti <t8230132@aueb.gr>
2026-05-15 20:11:30 +0300

    Created tests

--- tests/core/test_plotting.py
+++ tests/core/test_plotting.py
@@ -76,3 +76,55 @@ def test_plot_xy_without_metrics():
 
     assert len(ax.collections) > 0
     plt.close(fig)
+
+def test_plot_xy_accepts_single_metric():
+    X = np.linspace(0,5,10).reshape(-1,1)
+    y = X.flatten()
+    y_pred = y*1.1
+
+    fig, ax = plt.subplots()
+
+    plotting.plot_xy(
+        X,
+        y,
+        y_pred,
+        None,
+        ax=ax,
+        input_label="1",
+        output_label="2",
+        metrics = {"MAE":0.25}
+    )
+
+    text_values = [text.get_text() for text in ax.texts]
+
+    assert "MAE" in text_values
+    assert len(ax.collections) > 0
+
+    plt.close(fig)
+
+def test_plot_xy_accepts_multiple_metrics():
+    X = np.linspace(0,5,10).reshape(-1,1)
+    y = X.flatten()
+    y_pred = y * 1.1
+
+    fig, ax = plt.subplots()
+
+    plotting.plot_xy(
+        X,
+        y,
+        y_pred,
+        None,
+        ax=ax,
+        input_label = "1",
+        output_label= "2",
+        metrics = {"R2":0.5, "RMSE":0.1,"MAE":0.2}
+    )
+
+    text_values = [text.get_text() for text in ax.texts]
+
+    assert "R2" in text_values
+    assert "RMSE" in text_values
+    assert "MAE" in text_values
+    assert len(ax.collections) > 0
+
+    plt.close(fig)
